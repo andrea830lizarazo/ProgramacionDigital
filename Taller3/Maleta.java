e64c6f4bd1b7ae1db42b69eb5911770bdca884ab
andrea830lizarazo <andrea.lizarazosuarez@gmail.com>
2026-07-18 10:12:42 -0500

    .

--- Taller3/Maleta.java
+++ Taller3/Maleta.java
@@ -5,6 +5,7 @@ public class Maleta{
 		this.color=color;
 	}
 	public void recoger_maleta(){
-		System.out.println("La persona recoge la maleta de " +color);
+		System.out.println("La persona recoge la maleta " +color);
 	}
 }
+
