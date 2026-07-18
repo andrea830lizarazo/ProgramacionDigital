b8e6be9e9d826add992be4ac6eff9887d3e95b02
andrea830lizarazo <andrea.lizarazosuarez@gmail.com>
2026-07-18 10:12:05 -0500

    .

--- Taller3/MotorAvion.java
+++ Taller3/MotorAvion.java
@@ -1,17 +1,17 @@
 public class MotorAvion{
 	double cilindraje;
 	int potencia;
-	
+
 	MotorAvion(double cilindraje, int potencia){
 		this.cilindraje=cilindraje;
 		this.potencia=potencia;
 	}
-	
+
 	public void acelerar(){
-		System.out.println("El motor esta revolucionado");
+		System.out.println("El motor de cilindraje " +cilindraje+ " esta acelerado");
 	}
+
 	public void ralentizar(){
-		System.out.println("El motor esta ralentizado");
+		System.out.println("El motor de potencia " + potencia+ " esta ralentizado");
 	}
 }
-	
