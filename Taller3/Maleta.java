3e15f7107d9f8b125fd5686cba28dccfff2d4f54
andrea830lizarazo <andrea.lizarazosuarez@gmail.com>
2026-07-18 08:57:07 -0500

    Fix formatting in Maleta constructor

--- Taller3/Maleta.java
+++ Taller3/Maleta.java
@@ -2,7 +2,7 @@ public class Maleta{
 	String color;
 
 	Maleta(String color){
-	this.color=color;
+		this.color=color;
 	}
 	public void recoger_maleta(){
 		System.out.println("La persona recoge la maleta de " +color);
