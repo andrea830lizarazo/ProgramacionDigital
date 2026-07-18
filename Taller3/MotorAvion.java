75b8a62c758d9447df29c67b4812c2eaf1454a3f
andrea830lizarazo <andrea.lizarazosuarez@gmail.com>
2026-07-18 09:12:07 -0500

    Rename constructor from Motor to MotorAvion

--- Taller3/MotorAvion.java
+++ Taller3/MotorAvion.java
@@ -1,10 +1,12 @@
 public class MotorAvion{
 	double cilindraje;
 	int potencia;
-	Motor(double cilindraje, int potencia){
+	
+	MotorAvion(double cilindraje, int potencia){
 		this.cilindraje=cilindraje;
 		this.potencia=potencia;
 	}
+	
 	public void revolucionar(){
 		System.out.println("El motor esta revolucionado");
 	}
